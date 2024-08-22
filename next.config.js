a766082728e4943b185a70996df02baa883ab5f6
ejjem <tigonlee0627@gmail.com>
2024-08-22 16:44:29 +0900

    배포 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',  // 정적 HTML로 내보내기 설정 추가
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md/,
